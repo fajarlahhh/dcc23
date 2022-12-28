1447bab14387a3e7486059915258d3ab61063de1
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-12-28 22:21:12 +0800

    logout

--- app/Http/Livewire/Downline.php
+++ app/Http/Livewire/Downline.php
@@ -20,7 +20,7 @@ class Downline extends Component
     public function booted()
     {
         $this->network = UserView::with('downline.downline.downline')->with('invalidLeft')->with('invalidRight')->select(
-            '*s',
+            '*',
             DB::raw('(select ifnull(sum(package * reinvest), 0) from user_view uv where uv.activated_at is not null and left(uv.network, length(concat(user_view.network, user_view.id, "l")))=concat(user_view.network, user_view.id, "l") ) valid_left'),
             DB::raw('(select ifnull(sum(package * reinvest), 0) from user_view uv where uv.activated_at is not null and left(uv.network, length(concat(user_view.network, user_view.id, "r")))=concat(user_view.network, user_view.id, "r") ) valid_right')
         )->where('id', $this->key)->first();
