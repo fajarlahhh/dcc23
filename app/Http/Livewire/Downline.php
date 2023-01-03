d87fef3f44fe825404b776a4d493df30c32772b9
Andi Fajar Nugraha <andifajarlah@gmail.com>
2023-01-03 23:00:40 +0800

    baru

--- app/Http/Livewire/Downline.php
+++ app/Http/Livewire/Downline.php
@@ -21,8 +21,8 @@ class Downline extends Component
     {
         $this->network = UserView::with('downline.downline.downline')->with('invalidLeft')->with('invalidRight')->select(
             '*',
-            DB::raw('(select ifnull(sum(package * reinvest), 0) from user_view uv where uv.activated_at is not null and left(uv.network, length(concat(user_view.network, user_view.id, "l")))=concat(user_view.network, user_view.id, "l") ) valid_left'),
-            DB::raw('(select ifnull(sum(package * reinvest), 0) from user_view uv where uv.activated_at is not null and left(uv.network, length(concat(user_view.network, user_view.id, "r")))=concat(user_view.network, user_view.id, "r") ) valid_right')
+            DB::raw('(select ifnull(sum(package * reinvest), 0) from user_view uv where uv.network is not null and left(uv.network, length(concat(user_view.network, user_view.id, "l")))=concat(user_view.network, user_view.id, "l") ) valid_left'),
+            DB::raw('(select ifnull(sum(package * reinvest), 0) from user_view uv where uv.network is not null and left(uv.network, length(concat(user_view.network, user_view.id, "r")))=concat(user_view.network, user_view.id, "r") ) valid_right')
         )->where('id', $this->key)->first();
     }
 
